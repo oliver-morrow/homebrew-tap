c5a7f54e39a1851759bbc46b428d26ea8f568f67
Oliver Morrow <83565270+oliver-morrow@users.noreply.github.com>
2026-08-09 23:50:11 -0400

    fix PageScript Homebrew formula

--- Formula/pagescript.rb
+++ Formula/pagescript.rb
@@ -1,6 +1,9 @@
 class Pagescript < Formula
-  desc "LLM-native web composition compiler"
+  desc "Compile compact .page files to standalone HTML"
   homepage "https://github.com/oliver-morrow/pagescript"
+  url "https://github.com/oliver-morrow/pagescript/archive/refs/tags/v1.1.0-alpha.1.tar.gz"
+  sha256 "5980a95d279b560a2e9de2e3dd23906afc8c23e1cb36049695396da88a0d9ead"
+  license "MIT"
   head "https://github.com/oliver-morrow/pagescript.git", branch: "main"
 
   depends_on "rust" => :build
@@ -21,11 +24,9 @@ class Pagescript < Formula
       ::/page
     EOS
 
-    # Run validation
-    system bin/"pagescript-rs", "validate", "test.page"
-    
-    # Run rendering and check output
-    output = shell_output("#{bin}/pagescript-rs render test.page")
+    system bin/"pagescript", "validate", "test.page"
+
+    output = shell_output("#{bin}/pagescript render test.page")
     assert_match "Hello Brew", output
   end
 end
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # homebrew-tap
+
+## PageScript
+
+Install the current PageScript alpha:
+
+```sh
+brew install oliver-morrow/tap/pagescript
+```
